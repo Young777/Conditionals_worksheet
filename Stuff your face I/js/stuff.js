8ff01829c57fd692d70e7d1dca23cb8178de573b
Shaylen Young <young777@fullsail.edu>
2013-06-19 20:14:21 -0500

    stuff your face set up

--- Stuff your face I/js/stuff.js
+++ Stuff your face I/js/stuff.js
@@ -1,7 +1,13 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Young
- * Date: 6/19/13
- * Time: 7:53 PM
- * To change this template use File | Settings | File Templates.
- */
+//Are You Hungry?
+//Strawberry Festival’s pie eating contest
+
+var weightReq = 250; //Weight requirement to enter contest
+var CompWeight = 273;
+
+//if contestant is over 250 he can enter the contest
+
+if(weightReq > CompWeight){
+    //you can enter the contest!
+    console.log(“You qualify for the heavyweight division”)
+}
+
