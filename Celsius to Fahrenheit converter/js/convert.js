a7262a6718594a8544b10c3a1aac0cb9049c5e34
Shaylen Young <young777@fullsail.edu>
2013-06-19 23:23:29 -0500

    Converter complete

--- Celsius to Fahrenheit converter/js/convert.js
+++ Celsius to Fahrenheit converter/js/convert.js
@@ -1,1 +1,11 @@
 //Celsius to Fahrenheit converter
+
+// Degrees Celsius and Fahrenheit
+var C = 25;
+var F = 77;
+
+if(C === F){
+    console.log("The temperature is X degrees Fahrenheit.");
+}else{
+    console.log("The temperature is X degrees Celsius.");
+}
