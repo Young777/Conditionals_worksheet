42efd971d07ce327f8027e686671c0fdc81e2c57
Shaylen Young <young777@fullsail.edu>
2013-06-20 22:20:44 -0500

    login complete

--- Check the Login/js/login.js
+++ Check the Login/js/login.js
@@ -10,8 +10,9 @@ var Cpassword = password3
 if(username && password === Cusername && Cpassword){
     //if they match user will get welcome message
     console.log("Welcome, (Young777)!")
-}else if(username == Cusername){
-    console.log("User not found. Try again")
+}else if(username ==+ Cusername){
+    console.log("User not found. Try again");
 }else{
-    //
+    //if they do not match it will display error
+    console.log("Password does not match our records.");
 }
