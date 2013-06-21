9ccacbdb37740f97ba13c9eba7e96e82e1dc6637
Shaylen Young <young777@fullsail.edu>
2013-06-20 22:28:11 -0500

    pass

--- Tire Pressure I/js/tire.js
+++ Tire Pressure I/js/tire.js
@@ -3,8 +3,11 @@
 
 var tirePressure = [22, 24, 19, 22]
 
-//Time to calculate tire pressure
+    //Time to calculate tire pressure
+    //If any tire is lower than 20 you fail inspection
+if(tirePressure > 20){
+    console.log("The tires pass spec!")
+}else{
+    //if all tires are more than 20 you pass
 
-if(tirePressure > 24){
-    console.log()
 }
