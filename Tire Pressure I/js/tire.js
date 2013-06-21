e4ce30bc18385524bb8b9d68dd0613f54e078540
Shaylen Young <young777@fullsail.edu>
2013-06-20 22:26:51 -0500

    tire pressure

--- Tire Pressure I/js/tire.js
+++ Tire Pressure I/js/tire.js
@@ -1,7 +1,10 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Young
- * Date: 6/20/13
- * Time: 10:22 PM
- * To change this template use File | Settings | File Templates.
- */
+//Shaylen Young - 06/20 - Conditional
+//Check tire pressure
+
+var tirePressure = [22, 24, 19, 22]
+
+//Time to calculate tire pressure
+
+if(tirePressure > 24){
+    console.log()
+}
